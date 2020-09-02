0609859c9e2adca11858cf6a3b87cb959f30bc6b
BisiDavi <oludavidconnect@gmail.com>
2020-09-02 02:10:19 +0100

    Updates

---
+++ precache-manifest.b472f4ff1267134b87d087b0837ab100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdf38b33375781e1b6e89a8b912fa22",
+    "url": "/bisidavi.github.io/JJ/index.html"
+  },
+  {
+    "revision": "98dc93565b9532d428e5",
+    "url": "/bisidavi.github.io/JJ/static/css/main.6bd67ee6.chunk.css"
+  },
+  {
+    "revision": "9046be0bda6a2ff0f45f",
+    "url": "/bisidavi.github.io/JJ/static/js/2.76895bc2.chunk.js"
+  },
+  {
+    "revision": "0ec9300b90a1964ccb21b13048e7fb5c",
+    "url": "/bisidavi.github.io/JJ/static/js/2.76895bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98dc93565b9532d428e5",
+    "url": "/bisidavi.github.io/JJ/static/js/main.d757e403.chunk.js"
+  },
+  {
+    "revision": "4e776a398817623eae63",
+    "url": "/bisidavi.github.io/JJ/static/js/runtime-main.01e73de7.js"
+  }
+]);
